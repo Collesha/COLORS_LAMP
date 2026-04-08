67a5a96185873885e06735064acaa7031f8ed449
Collesha Mighty <c.mighty21@gmail.com>
2026-04-07 21:29:36 -0400

    Updated ESLint

---
+++ .eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                md5: "readonly"
+            }
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-console": "off"
+        }
+    }
+];
